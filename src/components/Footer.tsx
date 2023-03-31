a610ac4cdc1cc6062f5fb35b9ef32f038bd9b891
DarkNoriss <darknoriss@gmail.com>
2023-03-31 21:25:38 +0200

    more ts, fixing src

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,11 +1,12 @@
+import { FC } from 'react';
 import { PATHICONS } from '../constants';
 import '../styles/footer.scss';
 
-export const Footer = () => {
+export const Footer: FC = (): JSX.Element => {
   return (
     <footer className="footer">
-      <a className="footer-link">
-        <img src="./icons/github.svg"></img>
+      <a className="footer-link" href="https://github.com/DarkNoriss" target="__blank">
+        <img src={`${PATHICONS}github.svg`}></img>
         <p>DarkNoriss</p>
       </a>
     </footer>
