02ca2c37491cda1d8ba0de9c8af99dfdaaba6711
DarkNoriss <darknoriss@gmail.com>
2023-03-31 06:49:49 +0200

    info section

---
+++ src/components/PersonalInformation.tsx
@@ -0,0 +1,16 @@
+export const PersonalInformation = () => {
+  return (
+    <>
+      <h3>Personal Information</h3>
+      <div>
+        <input name="firstName" placeholder="First name"></input>
+        <input name="lastName" placeholder="Last name"></input>
+        <input name="phoneNumber" placeholder="Phone number"></input>
+        <input name="email" placeholder="Email"></input>
+        <input name="address" placeholder="Address"></input>
+        <input name="github" placeholder="GitHub link"></input>
+        <textarea name="desc" placeholder="About me"></textarea>
+      </div>
+    </>
+  );
+};
