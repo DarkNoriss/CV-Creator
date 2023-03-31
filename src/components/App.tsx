f67cebad2306421865647633750477ee92197094
DarkNoriss <darknoriss@gmail.com>
2023-03-31 06:49:16 +0200

    main component

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,12 +1,25 @@
+import { useState } from 'react';
 import { Header } from './Header';
+import { SectionList } from './SectionList';
+import { Footer } from './Footer';
+import '../styles/app.scss';
+import { PersonalInformation } from './PersonalInformation';
+import { Experience } from './Experience';
+import { Education } from './Education';
 
 export const App = () => {
-  console.log('hey typescript!');
+  const [sections] = useState([<PersonalInformation />, <Experience />, <Education />]);
+
   return (
     <>
       <Header />
-      <div className="app"></div>
-      {/* <Footer /> */}
+      <div className="app">
+        <div className="section-list">
+          <SectionList sections={sections} />
+        </div>
+        <div className="app-preview"></div>
+      </div>
+      <Footer />
     </>
   );
 };
