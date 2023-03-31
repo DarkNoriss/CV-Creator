e17e4800b7d8d04d27c10825ff9f70c5f5963cdd
DarkNoriss <darknoriss@gmail.com>
2023-03-31 06:48:26 +0200

    header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import '../styles/header.scss';
 export const Header = () => {
   return (
     <header className="header">
-      <h1>Todo list</h1>
+      <h1>CV Creator</h1>
     </header>
   );
 };
