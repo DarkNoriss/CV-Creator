c57f1dd8111191c265d8aa3d1fb359ce4f4eb5ad
DarkNoriss <darknoriss@gmail.com>
2023-03-31 06:48:56 +0200

    section list to create all sections

---
+++ src/components/SectionList.tsx
@@ -0,0 +1,19 @@
+import React, { ReactElement } from 'react';
+
+interface SectionListProps {
+  sections: ReactElement[];
+}
+export const SectionList = ({ sections }: SectionListProps) => {
+  return (
+    <>
+      {sections.map((section, index) => (
+        <section key={index}>{section}</section>
+      ))}
+      <div className="buttons">
+        <button className="generate">Generate PDF</button>
+        <button className="load">Load Example</button>
+        <button className="delete">Reset</button>
+      </div>
+    </>
+  );
+};
