ac045e1d4d18f422533961d61d023dc475c163c5
DarkNoriss <darknoriss@gmail.com>
2023-03-31 03:27:59 +0200

    removing not needed utils for now

--- src/constants.tsx
+++ src/constants.tsx
@@ -1,4 +1,2 @@
-// export const PATHICONS =
-//   "https://raw.githubusercontent.com/DarkNoriss/WeatherApp/master/src/icons/";
-
-// export const LOCAL_STORAGE_KEY = "todoApp.tasks";
+ export const PATHICONS =
+   "https://raw.githubusercontent.com/DarkNoriss/WeatherApp/master/src/icons/";
