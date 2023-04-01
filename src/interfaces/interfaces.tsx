c017e110ec5efff852c3a6ff77db5ea8a2dc090c
DarkNoriss <darknoriss@gmail.com>
2023-04-01 22:05:50 +0200

    cleaning

--- src/interfaces/interfaces.tsx
+++ src/interfaces/interfaces.tsx
@@ -1,17 +1,3 @@
-export interface ExperienceTypes {
-  position: string;
-  company: string;
-  city: string;
-  from: string;
-  to: string;
-  [key: string]: string;
-}
-
 export interface Props {
   updateData: (data: any, from: string) => void;
-}
-
-export interface SectionTypes {
-  personal: [];
-  experience: ExperienceTypes[];
 }
