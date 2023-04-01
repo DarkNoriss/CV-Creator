6aa1dd19b06c0cea22cbd84539114da75cf4467b
DarkNoriss <darknoriss@gmail.com>
2023-04-01 22:05:06 +0200

    moving functions to new file

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -1,64 +1,28 @@
-import { ChangeEvent, useState } from 'react';
-import { ExperienceTypes, Props } from '../interfaces/interfaces';
+import { useState } from 'react';
+import { Props } from '../interfaces/interfaces';
 import { useUpdateEffect } from '../utils/useUpdateEffect';
+import { createInput, addInput } from '../utils/createInput';
 
 export const Experience = ({ updateData }: Props) => {
-  const [exp, setExp] = useState<ExperienceTypes[]>(() => []);
+  const [exp, setExp] = useState<{ [key: string]: string }[]>([]);
   const fieldNames = ['position', 'company', 'city', 'from', 'to'];
 
   useUpdateEffect(() => {
     updateData(exp, 'experience');
   }, [exp]);
 
-  const handleChange = (event: ChangeEvent<HTMLInputElement>, index: number) => {
-    const { name, value } = event.target;
-
-    const updatedExp = exp.map((item, i) => (i !== index ? item : { ...item, [name]: value }));
-
-    setExp(updatedExp);
-  };
-
-  const addExp = () => {
-    const fieldNamesInit = fieldNames.reduce(
-      (acc, fieldName) => ({ ...acc, [fieldName]: '' as string }),
-      {} as ExperienceTypes
-    );
-
-    setExp([...exp, fieldNamesInit]);
-  };
-
-  const removeExp = (index: number) => {
-    setExp(exp.filter((_, i) => i !== index));
-  };
-
-  const renderExp = (index: number) => {
-    const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-      handleChange(e, index);
-    };
-
-    return (
-      <div key={index}>
-        {fieldNames.map((name) => (
-          <input
-            key={name}
-            name={name}
-            placeholder={name[0].toUpperCase() + name.slice(1)}
-            value={exp[index][name]}
-            onChange={handleInputChange}
-          />
-        ))}
-        <button onClick={() => removeExp(index)}>Delete</button>
-      </div>
-    );
+  const handleClick = () => {
+    addInput(exp, setExp, fieldNames);
   };
 
   return (
-    <section>
+    <>
       <h3>Experience</h3>
       <div>
-        {exp.length !== 0 && exp.map((_, index) => renderExp(index))}
-        <button onClick={addExp}>Add</button>
+        {exp.length !== 0 &&
+          exp.map((_, index) => createInput(exp, setExp, fieldNames, index, true))}
+        <button onClick={handleClick}>Add</button>
       </div>
-    </section>
+    </>
   );
 };
