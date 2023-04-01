8d26521852ac081c47250632ea5efd431ecfe3de
DarkNoriss <darknoriss@gmail.com>
2023-04-01 02:37:05 +0200

    moving sections and useState for all sections data

--- src/components/SectionList.tsx
+++ src/components/SectionList.tsx
@@ -1,10 +1,27 @@
-import React, { FC, ReactElement } from 'react';
+import React, { FC, ReactElement, useState } from 'react';
+import { SectionTypes } from '../interfaces/interfaces';
+import { PersonalInformation } from './PersonalInformation';
+import { Experience } from './Experience';
+import { Education } from './Education';
+import { useUpdateEffect } from '../utils/useUpdateEffect';
 
-interface SectionListProps {
-  sections: ReactElement[];
-}
+export const SectionList = () => {
+  const [sectionsData, setSectionsData] = useState<SectionTypes[]>(() => []);
+  const [sections] = useState([
+    <PersonalInformation />,
+    <Experience updateData={updateSectionsData} />,
+    <Education />,
+  ]);
 
-export const SectionList = ({ sections }: SectionListProps) => {
+  function updateSectionsData(data: any, from: string) {
+    setSectionsData((prevSectionsData) => ({
+      ...prevSectionsData,
+      [from]: data,
+    }));
+  }
+
+  useUpdateEffect(() => console.log(sectionsData), [sectionsData]);
+
   return (
     <>
       {sections.map((section, index) => (
