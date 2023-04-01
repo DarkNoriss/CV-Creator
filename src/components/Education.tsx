69420c0ece32fa9b6e193ec8e3432ed75cb3966c
DarkNoriss <darknoriss@gmail.com>
2023-04-01 22:05:30 +0200

    creating edu section

--- src/components/Education.tsx
+++ src/components/Education.tsx
@@ -1,29 +1,28 @@
-import { FC } from 'react';
+import { useState } from 'react';
+import { Props } from '../interfaces/interfaces';
+import { useUpdateEffect } from '../utils/useUpdateEffect';
+import { addInput, createInput } from '../utils/createInput';
 
-export const Education: FC = () => {
-  const addEducation = () => {
-    return (
-      <>
-        <input name="universityName" placeholder="University name"></input>
-        <input name="city" placeholder="City"></input>
-        <input name="degree" placeholder="Degree"></input>
-        <input name="subject" placeholder="Subject"></input>
-        <input name="from" placeholder="From"></input>
-        <input name="to" placeholder="To"></input>
-        <button>Delete</button>
-      </>
-    );
+export const Education = ({ updateData }: Props) => {
+  const [edu, setEdu] = useState<string[]>(() => []);
+  const fieldNames = ['universityName', 'city', 'degree', 'subject', 'from', 'to'];
+
+  useUpdateEffect(() => {
+    updateData(edu, 'education');
+  }, [edu]);
+
+  const handleClick = () => {
+    addInput(edu, setEdu, fieldNames);
   };
 
-  const handleAddEducation = () => {};
-
   return (
-    <section>
+    <>
       <h3>Education</h3>
       <div>
-        {addEducation()}
-        <button>Add</button>
+        {edu.length !== 0 &&
+          edu.map((_, index) => createInput(edu, setEdu, fieldNames, index, true))}
+        <button onClick={handleClick}>Add</button>
       </div>
-    </section>
+    </>
   );
 };
