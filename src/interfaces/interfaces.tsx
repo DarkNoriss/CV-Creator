f56689dec0bb722889836ba1e273a5c9ed99ee89
DarkNoriss <darknoriss@gmail.com>
2023-04-01 02:37:15 +0200

    global interface

---
+++ src/interfaces/interfaces.tsx
@@ -0,0 +1,19 @@
+import { Dispatch, SetStateAction } from 'react';
+
+export interface ExperienceTypes {
+  position: string;
+  company: string;
+  city: string;
+  from: string;
+  to: string;
+  [key: string]: string;
+}
+
+export interface Props {
+  updateData: (data: any, from: string) => void;
+}
+
+export interface SectionTypes {
+  personal: [];
+  experience: ExperienceTypes[];
+}
