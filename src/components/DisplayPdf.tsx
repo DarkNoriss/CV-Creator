456b02b972514647de0efbd393b1c2e8a61d2541
DarkNoriss <darknoriss@gmail.com>
2023-04-05 02:59:56 +0200

    working on pdf

--- src/components/DisplayPdf.tsx
+++ src/components/DisplayPdf.tsx
@@ -8,35 +8,47 @@ type DisplayPdfProps = {
 export const DisplayPdf = ({ sectionsData }: DisplayPdfProps) => {
   const { personalInformation, experience, education }: any = sectionsData;
 
-  console.log(personalInformation);
+  const getInfo = (label: string) => {
+    const section = personalInformation.find((info: any) => info[label]);
+    const info = section ? section[label] : '';
+    return info;
+  };
+
+  const getExp = (label: string) => {
+    return experience[label];
+  };
+
+  const getEdu = (label: string) => {
+    return education[label];
+  };
+
   return (
     <div className="pdf">
-      <div className="pdf-background">
-        <div className="bg-bg1"></div>
-        <div className="bg-bar"></div>
-        <div className="bg-bg2"></div>
+      <div className="pdf-bar">
+        <img></img>
+        <div className="desc ">
+          {personalInformation?.[0]?.desc && (
+            <>
+              <h3>PROFILE</h3>
+              <p>{getInfo('desc')}</p>
+            </>
+          )}
+        </div>
+        <div className="contact ">
+          <h3>CONTACT ME</h3>
+          <p>{personalInformation?.[0]?.phoneNumber && getInfo('phoneNumber')}</p>
+          <p>{personalInformation?.[0]?.email && getInfo('email')}</p>
+          <p>{personalInformation?.[0]?.address && getInfo('address')}</p>
+          <p>{personalInformation?.[0]?.github && getInfo('github')}</p>
+        </div>
       </div>
       <div className="pdf-header">
-        <div className="hder-bg1"></div>
-        <div className="hder-bar"></div>
-        <div className="hder-bg2">
-          <p>{personalInformation?.[0]?.firstName}</p>
-          <p>{personalInformation?.[0]?.lastName}</p>
+        <div className="pdf-header-text">
+          <p>{personalInformation?.[0]?.firstName && getInfo('firstName').toUpperCase()}</p>
+          <p>{personalInformation?.[0]?.lastName && getInfo('lastName').toUpperCase()}</p>
         </div>
       </div>
-      <div className="pdf-main">
-        <div className="main-bg1"></div>
-        <div className="main-bar"></div>
-        <div className="main-bg2">
-          <div className="exp">{experience?.[0] && <p className="head">Experience</p>}</div>
-          <div className="edu">{education?.[0] && <p className="head">Education</p>}</div>
-        </div>
-      </div>
+      <div className="pdf-text"></div>
     </div>
   );
 };
-
-// {dataNotEmpty() && <p>XD</p>}
-// education
-// experience
-// personalInformation
